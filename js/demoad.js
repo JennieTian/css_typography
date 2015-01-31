f11881234e82ca045e848897c68c899453ddfde9
JennieTian <jennietian@gmail.com>
2015-01-30 16:39:54 -0800

    css typography

---
+++ js/demoad.js
@@ -0,0 +1,90 @@
+/*global jQuery */
+/*!	
+ * Lettering.JS 0.6.1
+ *
+ * Copyright 2010, Dave Rupert http://daverupert.com
+ * Released under the WTFPL license
+ * http://sam.zoy.org/wtfpl/
+ *
+ * Thanks to Paul Irish - http://paulirish.com - for the feedback.
+ *
+ * Date: Mon Sep 20 17:14:00 2010 -0600
+ */
+(function($){
+    function injector(t, splitter, klass, after) {
+        var a = t.text().split(splitter), inject = '';
+        if (a.length) {
+            $(a).each(function(i, item) {
+                inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+            });
+            t.empty().append(inject);
+        }
+    }
+
+    var methods = {
+        init : function() {
+
+            return this.each(function() {
+                injector($(this), '', 'char', '');
+            });
+
+        },
+
+        words : function() {
+
+            return this.each(function() {
+                injector($(this), ' ', 'word', ' ');
+            });
+
+        },
+
+        lines : function() {
+
+            return this.each(function() {
+                var r = "eefec303079ad17405c889e092e105b0";
+                // Because it's hard to split a <br/> tag consistently across browsers,
+                // (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash
+                // (of the word "split").  If you're trying to use this plugin on that
+                // md5 hash string, it will fail because you're being ridiculous.
+                injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
+            });
+
+        }
+    };
+
+    $.fn.lettering = function( method ) {
+        // Method calling logic
+        if ( method && methods[method] ) {
+            return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
+        } else if ( method === 'letters' || ! method ) {
+            return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
+        }
+        $.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
+        return this;
+    };
+
+})(jQuery);
+
+var filename='http://tympanus.net/codrops/adpacks/demoadpacks.css?' + new Date().getTime();
+var fileref=document.createElement("link");
+fileref.setAttribute("rel", "stylesheet");
+fileref.setAttribute("type", "text/css");
+fileref.setAttribute("href", filename);
+document.getElementsByTagName("head")[0].appendChild(fileref);
+
+var demoad = document.createElement('div');
+demoad.id = 'cdawrap';
+demoad.innerHTML = '<span id="cda-remove"></span>';
+document.getElementsByTagName('body')[0].appendChild(demoad);
+
+document.getElementById('cda-remove').addEventListener('click',function(e){
+    demoad.style.display = 'none';
+    e.preventDefault();
+});
+
+var bsa = document.createElement('script');
+bsa.type = 'text/javascript';
+bsa.async = true;
+bsa.id = '_carbonads_js';
+bsa.src = '//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=codrops';
+demoad.appendChild(bsa);
